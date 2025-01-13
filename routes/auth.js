3518e3fe97a215759db48743abbcb503bca453ab
chris <caaker.0@gmail.com>
2025-01-13 13:46:17 -0600

    auto-commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-//
-// authentication begins 
-//
 
 // auth/google begins the authentication request from the client by calling passport.authenticate
 const scope = {
@@ -11,30 +8,45 @@ const scope = {
 };
 router.get('/google', passport.authenticate('google', scope));
 
-//
-// google has fielded the request and responded
-//
+// google has fielded the request and responded - we will always respond to the requester
+function getHost(req, res, next) {
+  let host = req.protocol + '://' + req.get('host');
+  req.auth_options = { successRedirect: host + '/', failureRedirect: host + '/' };
+  console.log('DEBUG: route: google auth: getHost: successRedirect: ' + req.auth_options.successRedirect);
+  next();
+}
 
-// after authentication the user is directed to /auth/google/callback with options for success and failure
-const options = {
-  successRedirect: '/',
-  failureRedirect: '/login',
-};
-router.get('/google/callback', passport.authenticate('google', options));
+// to avoid using global we put the options on the request object and retreive here
+function authenticateWrap(req, res, next) {
+  passport.authenticate('google', req.auth_options)(req, res, next);
+}
 
-//
-// logout logic
-//
+router.get('/google/callback', getHost, authenticateWrap );
 
-// receives at /auth/logout and sends user to /
+function getHostAgain(req) {
+  return req.protocol + '://' + req.get('host') + '/';
+}
+
+// auth/logout begins the logout process
 router.route('/logout').get((req, res) => {
-
-  // passport update requires a callback to logout or error is thrown
-  req.logout((err) => {
-
-  });
-  res.redirect('/');
+  req.logout(() => {});
+  res.redirect(getHostAgain(req));
 });
 
 
+
 module.exports = router;
+
+
+// const options = {
+//   successRedirect: '/',
+//   failureRedirect: '/login',
+// };
+
+
+// // auth/logout begins the logout process
+// router.route('/logout').get((req, res) => {
+//   // passport update requires a callback to logout or error is thrown
+//   req.logout((err) => { });
+//   res.redirect('/');
+// });
