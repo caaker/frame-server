d2d7b145643f0c073806606bf6dd5ed8bf2d9a58
chris <caaker.0@gmail.com>
2025-01-16 15:33:09 -0600

    auto-commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,50 +1,47 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-/****************************************************************************************************/
 
-// note we need to send 'origin' to google, which will send it back to us below
+/*
+1.  First request comes from the browser
+/****************************************************************************************************/
 router.get('/google', (req, res, next) => {
-  const o4 = req.get('referer');
-  console.log('DEBUG: route: google auth: state being sent to google: ' + o4);
-  // console.log(req.headers);
+  console.log('DEBUG: routes: /auth/google:' + origin );
+  const origin = req.get('referer');
   const config = {
     scope: ['email', 'profile'],
-    state: o4
+    state: origin
   }
   passport.authenticate('google', config )(req, res, next);
 });
 
+
+/*
+2.  Second request comes from google auth server
 /****************************************************************************************************/
-
-// google traffic hits auth/google/callback which is seen as being sent from here
 router.get('/google/callback', getHost, authenticateWrap );
-
-// google has fielded the request and responded
 function getHost(req, res, next) {
-  // let host = req.protocol + '://' + req.get('host');
+  console.log('DEBUG: routes: /auth/google/callback: ' + host);
   let host = req.query.state;
   req.auth_options = { successRedirect: host, failureRedirect: host };
-  console.log('DEBUG: route: google auth: state received from google:  ' + req.auth_options.successRedirect);
   next();
 }
-
-// to avoid using global we put the options on the request object and retreive here
 function authenticateWrap(req, res, next) {
   passport.authenticate('google', req.auth_options)(req, res, next);
 }
 
-function getHostAgain(req) {
-  return req.get('referer');
-  // return req.protocol + '://' + req.get('host') + '/';
-}
 
+/*
+a.  logout
 /****************************************************************************************************/
-
-// auth/logout begins the logout process
 router.route('/logout').get((req, res) => {
+  console.log('DEBUG: routes: /auth/logout: ' + getHostAgain(req) );
   req.logout(() => {});
   res.redirect(getHostAgain(req));
 });
+function getHostAgain(req) {
+  return req.get('referer');
+}
+
 
 module.exports = router;
