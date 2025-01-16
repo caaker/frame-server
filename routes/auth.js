9c150c9753793c433bb310dc3133df94e1d3052b
chris <caaker.0@gmail.com>
2025-01-16 15:51:29 -0600

    auto-commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
 const passport = require('passport');
 
-
 /*
 1.  First request comes from the browser
 /****************************************************************************************************/
 router.get('/google', (req, res, next) => {
   const origin = req.get('referer');
-  console.log('DEBUG: routes: /auth/google:' + origin );
+  console.log('DEBUG: routes: /auth/google:' + origin);
   const config = {
     scope: ['email', 'profile'],
     state: origin
@@ -15,14 +14,13 @@ router.get('/google', (req, res, next) => {
   passport.authenticate('google', config )(req, res, next);
 });
 
-
 /*
 2.  Second request comes from google auth server
 /****************************************************************************************************/
 router.get('/google/callback', getHost, authenticateWrap );
 function getHost(req, res, next) {
+  const host = req.query.state;
   console.log('DEBUG: routes: /auth/google/callback: ' + host);
-  let host = req.query.state;
   req.auth_options = { successRedirect: host, failureRedirect: host };
   next();
 }
@@ -30,18 +28,16 @@ function authenticateWrap(req, res, next) {
   passport.authenticate('google', req.auth_options)(req, res, next);
 }
 
-
 /*
 a.  logout
 /****************************************************************************************************/
-router.route('/logout').get((req, res) => {
-  console.log('DEBUG: routes: /auth/logout: ' + getHostAgain(req) );
+router.get('/logout', (req, res) => {
+  console.log('DEBUG: routes: /auth/logout: ' + getHostAgain(req));
   req.logout(() => {});
   res.redirect(getHostAgain(req));
 });
 function getHostAgain(req) {
   return req.get('referer');
 }
-
 
 module.exports = router;
