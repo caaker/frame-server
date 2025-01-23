83d9aaef9aebf8776fd1e21d4c6aa7ce78e46b7f
chris <caaker.0@gmail.com>
2025-01-23 12:40:26 -0600

    auto-commit

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 // req.user will populate with undefined or an object with the user data if passport has authenticated
 router.route('/get').get((req, res) => {
-  console.log('DEBUG: routes: /users/get : ' + req.user);
-
   const send = req.user || false;
   res.status(200).json(send);
 });
