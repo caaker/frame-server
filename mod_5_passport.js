f3ada0fcc18beca20d12f979678cca8438aeec0c
caaker <caaker.0@gmail.com>
2025-02-06 15:36:51 -0600

    Update mod_5_passport.js

--- mod_5_passport.js
+++ mod_5_passport.js
@@ -4,8 +4,7 @@ const Passport = require('passport');
 const GoogleAuth = require('passport-google-oauth').OAuth2Strategy;
 const { getOrSaveUser, serialize, deserialize } = require('./mod_5_passport_helper');
 
-//    find the connection string and parse it
-
+// find the connection string and parse it
 let connection;
 if (process.env.PASSPORT) {
   connection = JSON.parse(process.env.PASSPORT).Google;
@@ -14,10 +13,10 @@ if (process.env.PASSPORT) {
   console.log('\x1b[31m' + 'DEBUG: passport: connection string not found' + '\x1b[30m');
 }
 
-//    with a connection string we can now configure passport and export it for further configuration
+// with a connection string we can now configure passport and export it for further configuration
 if(connection) {
-  const test = new GoogleAuth(connection, getOrSaveUser);
-  Passport.use('google', test);
+  const GAObject = new GoogleAuth(connection, getOrSaveUser);
+  Passport.use('google', GAObject);
   Passport.serializeUser(serialize);
   Passport.deserializeUser(deserialize);
 }
