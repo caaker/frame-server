f59bb5671c98eb5fe73e20864cfed9175125c6b6
caaker <caaker.0@gmail.com>
2025-02-06 15:17:14 -0600

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,26 +2,27 @@ const router = require('express').Router();
 const passport = require('passport');
 
 /*
-1.  First request comes from the browser
-/****************************************************************************************************/
+1. First request comes from the browser
+****************************************************************************************************/
 router.get('/google', (req, res, next) => {
-  const origin = req.get('referer');
+  // referer is set by the browser and contains the browser url
+  const referer = req.get('referer');
   console.log('DEBUG: routes: /auth/google: ');
   const config = {
     scope: ['email', 'profile'],
-    state: origin
+    state: referer
   }
   passport.authenticate('google', config )(req, res, next);
 });
 
 /*
-2.  Second request comes from google auth server
+2.  Second request comes from google server
 /****************************************************************************************************/
 router.get('/google/callback', getHost, authenticateWrap );
 function getHost(req, res, next) {
-  const host = req.query.state;
+  const referer = req.query.state;
   console.log('DEBUG: routes: /auth/google/callback: ');
-  req.auth_options = { successRedirect: host, failureRedirect: host };
+  req.auth_options = { successRedirect: referer, failureRedirect: referer };
   next();
 }
 function authenticateWrap(req, res, next) {
