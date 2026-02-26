15570d2bb2a13ecf247f5fc464acdf704b7d5b7b
caaker <caaker.0@gmail.com>
2026-02-26 15:48:16 -0600

    Update CORS origin to include new domain

--- mod_00_cors.js
+++ mod_00_cors.js
@@ -7,7 +7,7 @@ function exportCors(app) {
   // configure CORS for a specific domains - current origin is implied - https://frame-server-x8qw.onrender.com/
   // support all CRUD operations
   const corsOptions = {
-    origin: ['https://caaker.github.io', 'http://localhost:3000'],
+    origin: ['https://chris-temp-level-0.github.io', 'https://caaker.github.io', 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
