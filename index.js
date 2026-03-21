cc631f7c9b817f7e49a10008d6689bb96e380270
caaker <caaker.0@gmail.com>
2026-03-21 15:57:31 -0500

    ... rename ... set in styles

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import './global.js';
 import express from 'express';
 import { configureShutdown }    from './mod_00_shutdown.js';
 import { configureCORS }        from './mod_00_cors.js';
-import { startServer }          from './mod_01_server.js';
-import { startWebsocketServer } from './mod_02_ws.js';
 import { configureRedirect }    from './mod_01_redirect.js';
-import { session_instance }     from './mod_4_session.js';
-import { passport }             from './mod_5_pass.js';
+import { startServer }          from './mod_01_server.js';
+import { startWebsocketServer } from './mod_01_ws.js';
+import { session_instance }     from './mod_02_session.js';
+import { passport }             from './mod_03_pass.js';
 import { detectErrors }         from './mod_10_detectErrors.js';
 
 // configure and start
@@ -18,21 +18,23 @@ configureRedirect(app);
 const server = startServer(app);
 startWebsocketServer(server);
 
-// app it up
+// app
 app.use(express.json());
 app.use('/', express.static('./dist'));
 app.use(session_instance);
+
+// passport - uses mongodb
 app.use(passport.initialize());
 app.use(passport.session());
 
-// route it up
+// routes - uses mongodb - complete refactor here, but define that file karpathy talked about 
 const routes = require('./mod_6_routes');
 app.use('/articles',  routes.articles);
 app.use('/users',     routes.users);
 app.use('/auth',      routes.auth);
 app.use('/test',      routes.test);
 
-// never
-detectErrors(app)
+//
+detectErrors(app);
 
 
