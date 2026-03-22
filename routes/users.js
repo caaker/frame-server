fd7be319cb75afe6cc50712ca88185e5e7f464e1
caaker <caaker.0@gmail.com>
2026-03-22 14:30:43 -0500

    updated users.js

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,10 @@
-// console.logD('DEBUG: routes: users', 'cyan');
+import express from 'express';
 
-const express = require('express');
 const router = express.Router();
-
-// req.user will populate with undefined or an object with the user data if passport has authenticated
 router.route('/get').get((req, res) => {
   console.logD('DEBUG: routes: /users/get: ', 'blue');
   const send = req.user || false;
   res.status(200).json(send);
 });
 
-module.exports = router;
+export { router as users };
