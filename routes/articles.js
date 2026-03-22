d50cffd4b7f20fdfead942dc7333a75d9af80cc8
caaker <caaker.0@gmail.com>
2026-03-22 15:14:04 -0500

    refactoring

--- routes/articles.js
+++ routes/articles.js
@@ -1,27 +1,27 @@
 // console.logD('DEBUG: routes: articles', 'cyan');
 
 import { Router } from 'express';
-import DBM from '../mongo/mongoose.js';
+import { database } from '../mongo/mongoose.js';
 
 const router = Router();
 
 router.route('/get').get((req, res) => {
   console.logD('DEBUG: routes: /articles/get: ', 'blue');
-  DBM.getAllArticles().then( (results) => {
+  database.getAllArticles().then( (results) => {
     res.status(200).json(results);
   }).catch(errorHandler);
 });
 
 router.route('/add').post((req, res) => {
   console.logD('DEBUG: routes: /articles/add: ', 'blue');
-  DBM.saveArticle(req.body).then((val) => {
+  database.saveArticle(req.body).then((val) => {
     res.status(200).json(val);
   }).catch(errorHandler);
 });
 
 router.route('/put/:_id').put((req, res) => {
   console.logD('DEBUG: routes: /articles/put: ', 'blue');
-  DBM.updateArticle(req.params._id, req.body).then((val) => {
+  database.updateArticle(req.params._id, req.body).then((val) => {
     console.logD( val, 'blue' );
     res.status(200).json(val);
   }).catch(errorHandler);
@@ -29,14 +29,13 @@ router.route('/put/:_id').put((req, res) => {
 
 router.route('/delete/:_id').delete((req, res) => {
   console.logD('DEBUG: routes: /articles/delete: ', 'blue');
-  DBM.deleteArticle(req.params._id).then((val) => {
+  database.deleteArticle(req.params._id).then((val) => {
     res.status(200).json(val);
   }).catch(errorHandler);
 });
 
 function errorHandler(err) {
   console.logD('DEBUG: routes: articles crud: error:', 'red');
-  // console.logD(error);
 }
 
 export { router as articles };
