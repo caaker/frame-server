e6a314ecc08361a503c98fc04f5b71007f7bea86
caaker <caaker.0@gmail.com>
2026-03-22 15:51:22 -0500

    continuing to refactor

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import './global.js';
 import express from 'express';
+
+// modules
 import { configureShutdown }    from './mod_00_shutdown.js';
 import { configureCORS }        from './mod_00_cors.js';
 import { configureRedirect }    from './mod_00_redirect.js';
 import { startServer }          from './mod_01_server.js';
-import { startWebsocketServer } from './mod_01_ws.js';
+import { startWebsocketServer } from './mod_01_websocket.js';
 import { session_instance }     from './mod_02_session.js';
 import { passport }             from './mod_03_passport.js';
 import { detectErrors }         from './mod_10_detectErrors.js';
 
-
+// routes
 import { articles } from './routes/articles.js';
 import { auth }     from './routes/auth.js';
 import { users }    from './routes/users.js';
@@ -22,10 +24,6 @@ app.enable('trust proxy');
 configureCORS(app);
 configureRedirect(app);
 
-// start servers
-const server = startServer(app);
-startWebsocketServer(server);
-
 // app
 app.use(express.json());
 app.use('/', express.static('./dist'));
@@ -36,11 +34,14 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // routes
+app.use('/auth',      auth);
 app.use('/articles',  articles);
 app.use('/users',     users);
-app.use('/auth',      auth);
 app.use('/test',      test);
 
+// error handling
 detectErrors(app);
 
-
+// start servers
+const server = startServer(app);
+startWebsocketServer(server);
