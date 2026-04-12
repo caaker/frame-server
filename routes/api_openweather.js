efb7d6510e9503b6a2bedc731df5a68604314cca
caaker <caaker.0@gmail.com>
2026-04-12 17:45:07 +0000

    added openai

---
+++ routes/api_openweather.js
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+const router = Router();
+const OPENWEATHER_KEY = '78fd50d70e6c6a18205f31af5ff95107';
+
+router.route('/openweather').get(async (req, res) => {
+  const { lat, lon } = req.query;
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+export { router as openweather };
