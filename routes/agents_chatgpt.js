713822efdde7c72de9dcfec97629de48f4d60ffa
caaker <caaker.0@gmail.com>
2026-04-12 20:05:31 +0000

    fixed api key

--- routes/agents_chatgpt.js
+++ routes/agents_chatgpt.js
@@ -2,23 +2,37 @@ import OpenAI from 'openai';
 import { Router } from 'express';
 const router = Router();
 
-// const openai = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY,
-// });
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI
+});
 
-// router.post('/', async (req, res) => {
-//   try {
-//     const { message } = req.body;
-//     const response = await openai.responses.create({
-//       model: 'gpt-4.1-mini',
-//       input: message,
-//     });
-//     res.json({
-//       reply: response.output[0].content[0].text,
-//     });
-//   } catch (err) {
-//     res.status(500).json({ error: err.message });
-//   }
-// });
+router.post('/message', async (req, res) => {
+  try {
+    const { message } = req.body;
+    const response = await openai.responses.create({
+    model: 'gpt-5.3-mini',
+    input: [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'user', content: message }
+      ]
+    });
+    res.json({
+      reply: response.output_text
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
-export { router as agents_chatgpt };
+export { router as api_chatgpt };
+
+/*  models
+      model: 'gpt-5.3-mini',
+      model: 'gpt-5.3',
+
+
+    notets:
+      two methods to guide behavior
+        instructions:
+        role;
+*/
